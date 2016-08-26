790aa7d628d741fe4b81b35db7f6c27885f3ae88
Jamie Tyler <jamie@thetylers.me.uk>
2016-08-26 15:08:56 +0100

    Update port for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 // uppercase defines this as a constant i.e. should not be changed
-var PORT = 3000; 
+var PORT = process.env.PORT || 3000; 
 var middleware = require('./middleware.js');
 
 // Middleware must be at the top of the code or else it will not run
